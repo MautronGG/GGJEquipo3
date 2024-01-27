da6537c52f77414db61c462d283d967589a3e58d
MautronGG <mauiguardia@gmail.com>
2024-01-27 13:47:03 -0600

    inventory

---
+++ Assets/Mau/Scripts/PlayerInventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+  public List <GameObject> m_memeCardsArray;
+  public List <GameObject> m_accesoriesArray;
+  // Start is called before the first frame update
+
+  public bool AddToArray(List<GameObject> list, GameObject item)
+  {
+    if(list.Count < 5)
+    {
+      list.Add(item);
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+    
+  }
+  public bool RemoveFromArray(List<GameObject> list, GameObject item)
+  {
+    if (list.Count > 0)
+    {
+      list.Remove(item);
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
