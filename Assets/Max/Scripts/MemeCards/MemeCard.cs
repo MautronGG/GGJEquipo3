6239b10101e9595bd6d7f6a903b8c0e22285b204
Maximiliano <107960477+SecondBehemoth@users.noreply.github.com>
2024-01-28 03:48:42 -0600

    Revert "si"
    This reverts commit 2ea1f02fc2e655a70c728cae8137ed152fb76dd1, reversing
    changes made to 24bbd8540c9f2f4ac740651a5acae4ed16aba59b.

--- Assets/Max/Scripts/MemeCards/MemeCard.cs
+++ Assets/Max/Scripts/MemeCards/MemeCard.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class MemeCard : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] CardStats _cardStats;
     [SerializeField] TextMeshProUGUI _nameTxt;
     [SerializeField] Image _image;
@@ -47,53 +46,4 @@ public class MemeCard : MonoBehaviour
         _image.sprite = memeCard.sprite;
         _nameTxt.text = memeCard.memeName;
     }
-=======
-  [SerializeField] CardStats _cardStats;
-  [SerializeField] TextMeshProUGUI _nameTxt;
-  [SerializeField] Image _image;
-  [SerializeField] TextMeshProUGUI _stpStatTxt;
-  [SerializeField] TextMeshProUGUI _fnyStatTxt;
-  [SerializeField] TextMeshProUGUI _crngStatTxt;
-  [SerializeField] Sprite _stpSprite;
-  [SerializeField] Sprite _fnySprite;
-  [SerializeField] Sprite _crngSprite;
-  [SerializeField] Image _typeText;
-  private int _stpStat;
-  private int _fnyStat;
-  private int _crngStat;
-
-  private void OnEnable()
-  {
-    _cardStats.GenerateInformation();
-  }
-
-  public void SendStats(string statType, int stat)
-  {
-    if (statType == "stpStat")
-    {
-      _stpStat = stat;
-      _typeText.sprite = _stpSprite;
-      Debug.Log(statType + ": " + stat);
-    }
-    else if (statType == "fnyStat")
-    {
-      _fnyStat = stat;
-      _typeText.sprite = _fnySprite;
-      Debug.Log(statType + ": " + stat);
-    }
-    else if (statType == "crngStat")
-    {
-      _typeText.sprite = _crngSprite;
-      _crngStat = stat;
-      Debug.Log(statType + ": " + stat);
-    }
-  }
-
-  public void SpriteAndName(MemeCardScriptable memeCard)
-  {
-    Debug.Log(memeCard);
-    _image.sprite = memeCard.sprite;
-    _nameTxt.text = memeCard.memeName;
-  }
->>>>>>> Mau
 }
