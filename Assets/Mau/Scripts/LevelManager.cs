765f789e05d41477b40903673d95813a9199b688
MautronGG <mauiguardia@gmail.com>
2024-01-28 06:29:11 -0600

    death?trigger, lights on level

--- Assets/Mau/Scripts/LevelManager.cs
+++ Assets/Mau/Scripts/LevelManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+  float time = 0f;
+  bool gameOn = false;
+  bool combat = false;
+  bool onCombat = false;
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +17,41 @@ public class LevelManager : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    if(onCombat)
+    {
+      //Smth Smth
+    }
+    else if(gameOn)
+    {
+      time += Time.deltaTime;
+      if (time >= 180)
+      {
+        gameOn = false;
+        combat = true;
+        time = 0f;
+        //Desactivar ActionMap
+        Cursor.lockState = CursorLockMode.None;
+      }
+    }
+    else if(!gameOn && !combat)
+    {
+      time += Time.deltaTime;
+      if (time >= 20)
+      {
+        gameOn = true;
+        time = 0f;
+      }
+    }
+    else if(combat)
+    {
+      time += Time.deltaTime;
+      if(time >= 20)
+      {
+        //Activar canvas de combate
+        combat = false;
+        onCombat = true;
+        time = 0f;
+      }
+    }
   }
 }
