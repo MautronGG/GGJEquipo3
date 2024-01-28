76c5f8225c45330ec264ed3e4e2515d0df621976
MautronGG <mauiguardia@gmail.com>
2024-01-28 03:08:46 -0600

    cards

--- Assets/Max/Scripts/MemeCards/MemeCard.cs
+++ Assets/Max/Scripts/MemeCards/MemeCard.cs
@@ -6,44 +6,51 @@ using UnityEngine.UI;
 
 public class MemeCard : MonoBehaviour
 {
-    [SerializeField] CardStats _cardStats;
-    [SerializeField] TextMeshProUGUI _nameTxt;
-    [SerializeField] Image _image;
-    [SerializeField] TextMeshProUGUI _stpStatTxt;
-    [SerializeField] TextMeshProUGUI _fnyStatTxt;
-    [SerializeField] TextMeshProUGUI _crngStatTxt;
-    private int _stpStat;
-    private int _fnyStat;
-    private int _crngStat;
+  [SerializeField] CardStats _cardStats;
+  [SerializeField] TextMeshProUGUI _nameTxt;
+  [SerializeField] Image _image;
+  [SerializeField] TextMeshProUGUI _stpStatTxt;
+  [SerializeField] TextMeshProUGUI _fnyStatTxt;
+  [SerializeField] TextMeshProUGUI _crngStatTxt;
+  [SerializeField] Sprite _stpSprite;
+  [SerializeField] Sprite _fnySprite;
+  [SerializeField] Sprite _crngSprite;
+  [SerializeField] Image _typeText;
+  private int _stpStat;
+  private int _fnyStat;
+  private int _crngStat;
 
-    private void OnEnable()
-    {
-        _cardStats.GenerateInformation();
-    }
+  private void OnEnable()
+  {
+    _cardStats.GenerateInformation();
+  }
 
-    public void SendStats(string statType, int stat)
+  public void SendStats(string statType, int stat)
+  {
+    if (statType == "stpStat")
     {
-        if (statType == "stpStat")
-        {
-            _stpStat = stat;
-            Debug.Log(statType + ": " + stat);
-        }
-        else if (statType == "fnyStat")
-        {
-            _fnyStat = stat;
-            Debug.Log(statType + ": " + stat);
-        }
-        else if (statType == "crngStat")
-        {
-            _crngStat = stat;
-            Debug.Log(statType + ": " + stat);
-        }
+      _stpStat = stat;
+      _typeText.sprite = _stpSprite;
+      Debug.Log(statType + ": " + stat);
     }
+    else if (statType == "fnyStat")
+    {
+      _fnyStat = stat;
+      _typeText.sprite = _fnySprite;
+      Debug.Log(statType + ": " + stat);
+    }
+    else if (statType == "crngStat")
+    {
+      _typeText.sprite = _crngSprite;
+      _crngStat = stat;
+      Debug.Log(statType + ": " + stat);
+    }
+  }
 
-    public void SpriteAndName(MemeCardScriptable memeCard)
-    {
-        Debug.Log(memeCard);
-        _image.sprite = memeCard.sprite;
-        _nameTxt.text = memeCard.memeName;
-    }
+  public void SpriteAndName(MemeCardScriptable memeCard)
+  {
+    Debug.Log(memeCard);
+    _image.sprite = memeCard.sprite;
+    _nameTxt.text = memeCard.memeName;
+  }
 }
